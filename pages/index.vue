bbd6a220fa28e5607702abfda6cf5b1a583bcfc5
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-07 20:19:17 +0530

    feat: add new column

--- pages/index.vue
+++ pages/index.vue
@@ -46,6 +46,16 @@ export default {
   },
 
   methods: {
+
+    // Save and get from Local Storage
+    getFromLocalStorage() {
+      localStorage.setItem('col', JSON.stringify(this.columns));
+      const newColumns = JSON.parse(localStorage.getItem('col'));
+
+      this.columns = newColumns;
+    },
+
+    // Add new Task
     addTask(col) {
       if (this.newTask !== "") {
         const newTaskObj = { id: 1, title: this.newTask };
@@ -53,10 +63,20 @@ export default {
 
         this.newTask = " ";
       }
+
+      this.getFromLocalStorage();
     },
 
-    newStatus() {
+    // Add new Status Column 
+    addStatus() {
+      if (this.newStatus !== "") {
+        this.columns[this.newStatus] = { tasks: [] };
 
+        this.getFromLocalStorage();
+      }
+
+      this.newStatus = " ";
+      console.log(this.columns);
     }
   }
 };
@@ -64,26 +84,38 @@ export default {
 
 <template>
   <div id="app">
-    <div class="flex justify-center">
+    <div class="pt-10 pl-10">
+      <!-- Add new Status Option -->
+      <input 
+        v-model="newStatus" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        type="text" placeholder="Add new Status">
+      <button 
+        class=" bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 
+        hover:border-blue-500 rounded mx-2" @click="addStatus">
+        Add New
+      </button>
+    </div>
+
+    <div class="flex justify-center relative">
       <div class="min-h-screen flex overflow-x-scroll py-12">
         <div v-for="key in Object.keys(columns)" :key="key"
           class="bg-gray-100 rounded-lg px-3 py-3 column-width rounded mr-4">
+          <!-- Status Title -->
           <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{ key }}</p>
-          
+
           <!-- Draggable component comes from vuedraggable. It provides drag & drop functionality -->
           <draggable :list="columns[key].tasks" :animation="200" ghost-class="ghost-card" group="tasks">
-            
+
             <!-- Each element from here will be draggable and animated. Note :key is very important here to be unique both for draggable and animations to be smooth & consistent. -->
-            <task-card v-for="(task) in columns[key].tasks" :key="task.id" :task="task" class="mt-3 cursor-move"></task-card>
-            
+            <task-card v-for="(task) in columns[key].tasks" :key="task.id" :task="task" class="mt-3 cursor-move">
+            </task-card>
           </draggable>
-          
+
           <br />
           <input v-model="newTask" type="text">
           <button @click="addTask(key)">Add</button>
+
         </div>
-        <!-- Add new Status Button -->
-        <button @click="newStatus">Add New</button>
       </div>
     </div>
   </div>
