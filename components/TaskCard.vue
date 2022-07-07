f225d78adc84dd7ec6fd164a14c95904b6e895e8
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-07 16:50:49 +0530

    Setup UI

---
+++ components/TaskCard.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    components: {
+    },
+    props: {
+        task: {
+            type: Object,
+            default: () => ({})
+        }
+    },
+    computed: {
+    }
+};
+</script>
+
+<template>
+    <div class="bg-white shadow rounded px-3 pt-3 pb-5 border border-white">
+        <div class="flex justify-between">
+            <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{task.title}}</p>
+
+            <img class="w-6 h-6 rounded-full ml-3"
+                src="https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png" alt="Avatar">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
