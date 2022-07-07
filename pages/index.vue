2cd6d2dd97f25f4855820cae035e0e34f5f9934c
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-07 18:18:10 +0530

    feat: add new new task for each status

--- pages/index.vue
+++ pages/index.vue
@@ -11,132 +11,181 @@ export default {
   data() {
     /*
     columns = [
-      
+      {
+        title: "First",
+        tasks: []
+      },
+      {
+        title: "First",
+        tasks: []
+      }
     ]
     */
+
+
     return {
-      columns: [
-        {
-          title: "Backlog",
+      columns : {
+        col1: {
           tasks: [
             {
               id: 1,
               title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request"
             },
             {
               id: 2,
               title: "Provide documentation on integrations",
-              date: "Sep 12"
             },
             {
               id: 3,
               title: "Design shopping cart dropdown",
-              date: "Sep 9",
-              type: "Design"
             },
-            {
-              id: 4,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request"
-            },
-            {
-              id: 5,
-              title: "Test checkout flow",
-              date: "Sep 15",
-              type: "QA"
-            }
           ]
         },
-        {
-          title: "In Progress",
-          tasks: [
-            {
-              id: 6,
-              title: "Design shopping cart dropdown",
-              date: "Sep 9",
-              type: "Design"
-            },
-            {
-              id: 7,
+        col2: {
+          tasks: [{
+              id: 1,
               title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request"
             },
             {
-              id: 8,
+              id: 2,
               title: "Provide documentation on integrations",
-              date: "Sep 12",
-              type: "Backend"
-            }
-          ]
-        },
-        {
-          title: "Review",
-          tasks: [
-            {
-              id: 9,
-              title: "Provide documentation on integrations",
-              date: "Sep 12"
             },
             {
-              id: 10,
+              id: 3,
               title: "Design shopping cart dropdown",
-              date: "Sep 9",
-              type: "Design"
-            },
-            {
-              id: 11,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request"
-            },
-            {
-              id: 12,
-              title: "Design shopping cart dropdown",
-              date: "Sep 9",
-              type: "Design"
-            },
-            {
-              id: 13,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request"
-            }
-          ]
-        },
-        {
-          title: "Done",
-          tasks: [
-            {
-              id: 14,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request"
-            },
-            {
-              id: 15,
-              title: "Design shopping cart dropdown",
-              date: "Sep 9",
-              type: "Design"
-            },
-            {
-              id: 16,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request"
-            }
-          ]
+            },]
         }
-      ]
+      }
     };
+     
+    // return {
+    //   columns: [
+    //     {
+    //       title: "Backlog",
+    //       tasks: [
+    //         {
+    //           id: 1,
+    //           title: "Add discount code to checkout page",
+    //           date: "Sep 14",
+    //           type: "Feature Request"
+    //         },
+    //         {
+    //           id: 2,
+    //           title: "Provide documentation on integrations",
+    //           date: "Sep 12"
+    //         },
+    //         {
+    //           id: 3,
+    //           title: "Design shopping cart dropdown",
+    //           date: "Sep 9",
+    //           type: "Design"
+    //         },
+    //         {
+    //           id: 4,
+    //           title: "Add discount code to checkout page",
+    //           date: "Sep 14",
+    //           type: "Feature Request"
+    //         },
+    //         {
+    //           id: 5,
+    //           title: "Test checkout flow",
+    //           date: "Sep 15",
+    //           type: "QA"
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       title: "In Progress",
+    //       tasks: [
+    //         {
+    //           id: 6,
+    //           title: "Design shopping cart dropdown",
+    //           date: "Sep 9",
+    //           type: "Design"
+    //         },
+    //         {
+    //           id: 7,
+    //           title: "Add discount code to checkout page",
+    //           date: "Sep 14",
+    //           type: "Feature Request"
+    //         },
+    //         {
+    //           id: 8,
+    //           title: "Provide documentation on integrations",
+    //           date: "Sep 12",
+    //           type: "Backend"
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       title: "Review",
+    //       tasks: [
+    //         {
+    //           id: 9,
+    //           title: "Provide documentation on integrations",
+    //           date: "Sep 12"
+    //         },
+    //         {
+    //           id: 10,
+    //           title: "Design shopping cart dropdown",
+    //           date: "Sep 9",
+    //           type: "Design"
+    //         },
+    //         {
+    //           id: 11,
+    //           title: "Add discount code to checkout page",
+    //           date: "Sep 14",
+    //           type: "Feature Request"
+    //         },
+    //         {
+    //           id: 12,
+    //           title: "Design shopping cart dropdown",
+    //           date: "Sep 9",
+    //           type: "Design"
+    //         },
+    //         {
+    //           id: 13,
+    //           title: "Add discount code to checkout page",
+    //           date: "Sep 14",
+    //           type: "Feature Request"
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       title: "Done",
+    //       tasks: [
+    //         {
+    //           id: 14,
+    //           title: "Add discount code to checkout page",
+    //           date: "Sep 14",
+    //           type: "Feature Request"
+    //         },
+    //         {
+    //           id: 15,
+    //           title: "Design shopping cart dropdown",
+    //           date: "Sep 9",
+    //           type: "Design"
+    //         },
+    //         {
+    //           id: 16,
+    //           title: "Add discount code to checkout page",
+    //           date: "Sep 14",
+    //           type: "Feature Request"
+    //         }
+    //       ]
+    //     }
+    //   ]
+    // };
   },
 
   methods: {
-    addTask() {
-      console.log("addtaskjj");
+    addTask(col) {
+      if (this.newTask !== "") {
+        const newTaskObj = { id: 1, title: this.newTask };
+        this.columns[col].tasks.push(newTaskObj);
+
+        this.newTask = " ";
+      }
     }
   }
 };
@@ -146,16 +195,19 @@ export default {
   <div id="app">
     <div class="flex justify-center">
       <div class="min-h-screen flex overflow-x-scroll py-12">
-        <div v-for="column in columns" :key="column.title"
+        <div v-for="key in Object.keys(columns)" :key="key"
           class="bg-gray-100 rounded-lg px-3 py-3 column-width rounded mr-4">
-          <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{ column.title }}</p>
+          <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{ key }}</p>
           <!-- Draggable component comes from vuedraggable. It provides drag & drop functionality -->
-          <draggable :list="column.tasks" :animation="200" ghost-class="ghost-card" group="tasks">
+          <draggable :list="columns[key].tasks" :animation="200" ghost-class="ghost-card" group="tasks">
             <!-- Each element from here will be draggable and animated. Note :key is very important here to be unique both for draggable and animations to be smooth & consistent. -->
-            <task-card v-for="(task) in column.tasks" :key="task.id" :task="task" class="mt-3 cursor-move"></task-card>
+            <task-card v-for="(task) in columns[key].tasks" :key="task.id" :task="task" class="mt-3 cursor-move">
+            </task-card>
             <!-- </transition-group> -->
           </draggable>
-          <input type="text" @keyup="addTask">
+          <br />
+          <input v-model="newTask" type="text">
+          <button @click="addTask(key)">Add</button>
         </div>
       </div>
     </div>
