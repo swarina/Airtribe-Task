f57a7999f41ff4da0942ae2681d3563300daca8e
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-07 20:43:42 +0530

    add: count of tasks

--- pages/index.vue
+++ pages/index.vue
@@ -102,7 +102,10 @@ export default {
         <div v-for="key in Object.keys(columns)" :key="key"
           class="bg-gray-100 rounded-lg px-3 py-3 column-width rounded mr-4">
           <!-- Status Title -->
-          <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{ key }}</p>
+          <p class="text-gray-800 font-bold font-sans tracking-wide text-lg px-1 pb-2">
+            {{ key }}
+            <span class="text-red-500" style="float: right">{{columns[key].tasks.length}}</span>
+          </p>
 
           <!-- Draggable component comes from vuedraggable. It provides drag & drop functionality -->
           <draggable :list="columns[key].tasks" :animation="200" ghost-class="ghost-card" group="tasks">
